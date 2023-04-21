7a55570d87e969e30772dfd1a101815307bbcf5b
Fixbound <53485556+Fixbound@users.noreply.github.com>
2023-04-21 11:27:34 -0300

    Add files via upload

---
+++ src/main/java/com/Project/SpringAngular/SpringAngularApplication.java
@@ -0,0 +1,27 @@
+package com.Project.SpringAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication @EnableWebMvc public class SpringAngularApplication {
+
+public static void main(String[] args) {
+    SpringApplication.run(SpringAngularApplication.class, args);
+}
+
+@Bean
+public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD");
+        }
+    };
+}
+}
